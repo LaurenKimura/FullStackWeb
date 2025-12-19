fdbe2a5479f49bc6d8658c93b7867df981f4c63b
LaurenKimura <laurenkimura23@gmail.com>
2025-12-18 19:42:51 -0800

    Added a like and dislike

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
+import { useState } from 'react'
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div  className = "card">
       <h2>{title}</h2>
+
+      <button onClick= {() => setHasLiked(!hasLiked)}>
+      {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 
 }
 
 const App = () => {
+
   return (
     //for style go to index.css .class-container
     <div className = "card-container"> 
