78211d364918ddb91cd978af128ff7f6387ceb5d
LaurenKimura <laurenkimura23@gmail.com>
2025-12-18 19:24:50 -0800

    Added default screen

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
 
+const Card = ({ title }) => {
+  return (
+    <div  className = "card">
+      <h2>{title}</h2>
+    </div>
+  )
+
+}
+
 const App = () => {
   return (
-    <h2>Arrow Compoent</h2>
+    //for style go to index.css .class-container
+    <div className = "card-container"> 
+      <Card title = "Avatar"/>
+      <Card title = "Home alone"/>
+      <Card title = "Space rangers"/>
+    </div>
   )
 
 }
