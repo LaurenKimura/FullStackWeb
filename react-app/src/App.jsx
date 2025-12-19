bf4b96df646f14dce7f0098afd488346e85279a5
LaurenKimura <laurenkimura23@gmail.com>
2025-12-18 19:49:56 -0800

    Added console log

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked, title]);
 
   return (
     <div  className = "card">
